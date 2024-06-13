6c0a73bc8ebb2f8addf9cf6618180904314d2a42
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-06-13 17:54:26 +0600

    fix the issue

--- src/services/articleServices.tsx
+++ src/services/articleServices.tsx
@@ -2,7 +2,6 @@
 
 import { RecentArticleDataType } from "@/types/RecentArticle";
 
-
 export async function fetchArticles({
   page,
   limit,
@@ -10,48 +9,33 @@ export async function fetchArticles({
   page: number;
   limit: number;
 }): Promise<{
-  data: RecentArticleDataType[]
+  data: RecentArticleDataType[];
   page: number;
   limit: number;
   total: number;
 }> {
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/api/v1/article/all?page=${page}&limit=${limit}`, {
-          cache: "no-store",
-        });
-      
-        if (!response.ok) {
-          throw new Error(`Failed to fetch articles: ${response.status} ${response.statusText}`);
-        }
-      
-        const data = await response.json();
-        return {
-          data: data.data,
-          page: data.meta.page,
-          limit: data.meta.limit,
-          total: data.meta.total,
-        };
-      } catch (error) {
-        console.error("Error fetching articles:", error);
-        throw new Error("Failed to fetch articles. Please try again later.");
-      }
-  // const [page,setPage] = useState<number>(0)
-//   const response = await fetch(
-//     `${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/api/v1/article/all?page=${page}&limit=${limit}`,
-//     {
-//       cache: "no-store", // To ensure the data is fetched on every request
-//     }
-//   );
-  
-//   if (!response.ok) {
-//     throw new Error("Failed to fetch articles");
-//     }
-    
-//     const data = await response.json(); 
-//   return {
-//     data: data.data,
-//     page: data.meta.page,
-//     limit: data.meta.limit,
-//     total: data.meta.total,
-//   }
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_SERVER_URL}/api/v1/article/all?page=${page}&limit=${limit}`
+    //   ,
+    //   {
+    //     cache: "no-store", // To ensure the data is fetched on every request
+    //   }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch articles");
+    }
+
+    const data = await response.json();
+    return {
+      data: data.data,
+      page: data.meta.page,
+      limit: data.meta.limit,
+      total: data.meta.total,
+    };
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    throw new Error("Failed to fetch articles. Please try again later.");
+  } 
 }
