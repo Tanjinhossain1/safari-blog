2ceecd48db023916321b9218f9431d034aba0a1c
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-06-26 03:29:48 +0600

    make news page

---
+++ src/Component/News/NewsComponent.tsx
@@ -0,0 +1,125 @@
+"use client";
+import Footer from "@/Component/HomePage/Footer";
+import Navbar from "@/Component/Shared/Navbar";
+import { RecentArticleDataType } from "@/types/RecentArticle";
+import {
+  Alert,
+  Breadcrumbs,
+  Button,
+  Container,
+  Grid,
+  Link,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import React, { Fragment, useEffect, useState } from "react";
+import DisplayArticleComponent from "../HomePage/DisplayArticleComponent";
+import { CategoryTypes } from "@/types/category"; 
+
+export default function NewsPageComponent({
+  articles,
+  total,  
+}: {
+  articles: RecentArticleDataType[];
+  total: number;  
+}) {
+  const params = useParams();
+  const history = useRouter();
+  const [isHideLoadMore, setIsHideLoadMore] = useState<boolean>(false);
+
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page") ?? "1";
+  const limit = searchParams.get("limit") ?? "3";
+  const search = searchParams.get("search") ?? "";
+
+  // Function to load more articles
+  const loadMoreArticles = async () => { 
+        history.push(
+            `/news?${new URLSearchParams({
+              page: page,
+              limit: `${Number(limit) + 2}`,
+            })}`,
+            {
+              scroll: false,
+            }
+          );
+    
+  };
+
+  useEffect(() => {
+    if (articles.length === total) {
+      setIsHideLoadMore(true);
+    }
+  }, [articles.length, total]);
+
+  return (
+    <>
+       
+      <Grid container>
+        <Grid xs={0} md={1} lg={1.1} xl={2}></Grid>
+        <Grid xs={12} md={10} lg={9.8} xl={8}>
+          <Paper sx={{ p: 2, mb: 2 }} elevation={2}> 
+
+            {articles && articles.length === 0 && (
+              <Alert severity="warning">
+                No Article Found For {params?.category}.
+              </Alert>
+            )} 
+            <Grid container>
+              <Grid xs={12} md={8}>
+                <Grid container>
+                  {articles &&
+                    articles?.map((data: RecentArticleDataType) => {
+                      console.log(data);
+                      return (
+                        <Fragment key={data.id}>
+                          <DisplayArticleComponent asSmall data={data} />
+                        </Fragment>
+                      );
+                    })}
+                </Grid>
+              </Grid> 
+            </Grid>
+            <Grid sx={{ mt: 3 }} container>
+        <Grid xs={1}></Grid>
+        <Grid xs={10} sm={4}>
+          {isHideLoadMore ? null : (
+            <Button
+              variant="outlined"
+              sx={{
+                borderColor: "#1976d2", // Border color
+                color: "#1976d2", // Text color
+                padding: "10px 20px",
+                fontSize: "16px",
+                width: "100%",
+                textTransform: "none",
+                transition:
+                  "border-color 0.3s ease-in-out, color 0.3s ease-in-out, transform 0.3s ease-in-out",
+                "&:hover": {
+                  borderColor: "#115293", // Darker shade for hover
+                  color: "#115293",
+                  transform: "scale(1.05)",
+                },
+                "&:active": {
+                  borderColor: "#4791db", // Lighter shade for active
+                  color: "#4791db",
+                  transform: "scale(0.95)",
+                },
+              }}
+              onClick={loadMoreArticles}
+            >
+              Load More
+            </Button>
+          )}
+        </Grid>
+        <Grid xs={1}></Grid>
+      </Grid>
+          </Paper>
+        </Grid>
+        <Grid xs={0} md={1} lg={1.1} xl={2}></Grid>
+      </Grid>
+      
+    </>
+  );
+}
---
+++ src/app/news/page.tsx
@@ -0,0 +1,25 @@
+import Footer from '@/Component/HomePage/Footer'
+import NewsPageComponent from '@/Component/News/NewsComponent'
+import Navbar from '@/Component/Shared/Navbar'
+import { fetchArticles } from '@/services/articleServices';
+import React, { Suspense } from 'react'
+
+interface NewsPagePropsType {
+    searchParams: {
+      page: string;
+      limit: string;
+    };
+  }
+
+export default async function NewsPage({searchParams}:NewsPagePropsType) {
+    const { page, limit } = searchParams;
+    const articles = await fetchArticles({ page, limit });
+
+  return (
+    <>
+   <Suspense> <Navbar /></Suspense>
+    <NewsPageComponent articles={articles.data} total={articles.total} />
+    <Suspense> <Footer /></Suspense>
+    </>
+  )
+}
