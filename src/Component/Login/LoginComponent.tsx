ff835b30fa6523986dfda8ecf37211c0d1ce7388
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-04 22:53:00 +0600

    make changese in ui

--- src/Component/Login/LoginComponent.tsx
+++ src/Component/Login/LoginComponent.tsx
@@ -36,6 +36,7 @@ const LoginComponent = () => {
           type="email"
           name="email"
           style={{marginBottom:"1 0px"}}
+          className=" text-black"
         />
 
         <label htmlFor="email" style={{color:"white"}}>Password</label>
@@ -44,7 +45,7 @@ const LoginComponent = () => {
           placeholder="*************"
           type="password"
           name="password"
-          className="mb-6"
+          className="mb-6 text-black"
         />
 
         <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">
