dbca34a50f827446aa0bebbcaa4c297b827cfdf8
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-05 02:18:46 +0600

    final all problem sove for build

--- src/Component/Shared/Navbar.tsx
+++ src/Component/Shared/Navbar.tsx
@@ -4,13 +4,13 @@ import NavbarHelper from "./NavbarHelperComponent";
 
 
 async function Navbar() {
-  // const session = await auth();
-  // const user = session?.user;
-  // console.log(session, "register  ", session,user);
+  const session = await auth();
+  const user = session?.user;
+  console.log(session, "register  ", session,user);
    
 
-  return (
-   <NavbarHelper isLoginUser={{fullName:"tanjin"}}  />
+  return (  
+   <NavbarHelper isLoginUser={user ? user : undefined}  /> 
   );
 }
 export default Navbar;
--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -1,15 +1,8 @@
-"use client"
-import { Container, Grid, Paper, Typography } from "@mui/material";
-import Image from "next/image";
 import React, { Fragment } from "react";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';  
-import { useRouter } from "next/navigation";
-import OtherPageFooter from "@/Component/Shared/Footer";
 import Navbar from "@/Component/Shared/Navbar";
 import AboutUsComponent from "./_components/AboutUsComponent";
 
 export default function AboutUs() {
-    const history = useRouter()
   return (
     <Fragment>
       <Navbar />
