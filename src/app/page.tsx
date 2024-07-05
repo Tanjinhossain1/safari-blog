93350fb73c5c78edb0e997aadaca40611cdbe910
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-05 17:50:15 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/Component/Shared/Navbar";
 import Banner from "@/Component/HomePage/Banner";
 import Footer from "@/Component/HomePage/Footer";
-import { fetchArticles, fetchBrands, fetchCategories } from "@/services/articleServices";
+import {
+  fetchArticles,
+  fetchBrands,
+  fetchCategories,
+} from "@/services/articleServices";
 import { Suspense } from "react";
 
 interface HomePropsType {
@@ -13,23 +17,32 @@ interface HomePropsType {
 async function Home({ searchParams }: HomePropsType) {
   const { page, limit } = searchParams;
   const articles = await fetchArticles({ page, limit });
-  const LatestArticles = await fetchArticles({ page, limit,latestDevice:"latest" });
+  const LatestArticles = await fetchArticles({
+    page,
+    limit,
+    latestDevice: "latest",
+  });
   const Category = await fetchCategories();
   const brands = await fetchBrands();
   return (
     <>
-       <Suspense>
-        <Navbar /> 
-      </Suspense>  
+      <Suspense>
+        <Navbar />
+      </Suspense>
       {articles ? (
         <Suspense>
-          <Banner brands={brands.data} latestArticles={LatestArticles.data} category={Category.data} articles={articles.data} total={articles.total} />
+          <Banner
+            brands={brands.data}
+            latestArticles={LatestArticles.data}
+            category={Category.data}
+            articles={articles.data}
+            total={articles.total}
+          />
         </Suspense>
-      ) : null}  
+      ) : null}
       <Suspense>
-      <Footer />
+        <Footer />
       </Suspense>
-        
     </>
   );
 }
